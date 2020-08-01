d1ebee8f8634647e30a6b257a65c3afa2b17a549
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-01 20:02:46 +0800

    simple project

---
+++ OOP/project.py
@@ -0,0 +1,17 @@
+name = input("Enter Name: ")
+age = input("Enter Age: ")
+gender = input("Enter Gender: ")
+
+
+if int(age) <= 0:
+    print("age must be 1 and up")
+else:
+    if gender != "male" and gender != "female":
+        print("there is an error in your input data")
+    else:
+        if gender.lower() == "male" and int(age) >= 13:
+            print("Hello", name, "You are handsome teenager.")
+        elif gender.lower() == "female" and int(age) >= 13:
+            print("Hi", name, "You are a beautiful teenager.")
+        elif gender.lower() == "male" or gender.lower() == "female" and int(age) <= 12:
+            print("Hello", name, "You are young")
